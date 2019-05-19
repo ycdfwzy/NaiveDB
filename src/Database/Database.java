ef176d0634111b5d7e59173b56874b68a8380ac7
wonderfulnx <naxinlegend@outlook.com>
2019-05-19 23:27:37 +0800

    fix something....

--- src/Database/Database.java
+++ src/Database/Database.java
@@ -5,6 +5,7 @@ import utils.MyLogger;
 import java.io.*;
 import java.util.*;
 import java.util.logging.*;
+import Table.Table;
 
 public class Database {
 
@@ -12,12 +13,19 @@ public class Database {
     private String dbPath;
     public Logger logger;
 
-    // public static void drop()
-
     public Database(String db_name) throws Exception {
         this.dbName = db_name;
         this.dbPath = DatabaseManager.getDatabasePath(db_name);
         this.logger = MyLogger.getLogger(db_name);
     }
+
+    public String getName(){ return this.dbName; }
+
+    // public Table getTable(String tb_name) throws Exception{
+    //     return new Table("lalaa", null);
+
+    // }
+
+    // public Table createTable(String table_name, ArrayList<Pair<String, Type>> cols)
 
 }
--- src/Table/Table.java
+++ src/Table/Table.java
@@ -5,22 +5,30 @@ import Table.Type;
 import java.io.*;
 import java.util.*;
 import javafx.util.Pair;
-import Database.DatabaseManager;
 
 public class Table {
 
     protected String tableName;
+    protected String baseDir;
     protected BPlusTree index;
     protected File metaFile;
     protected RandomAccessFile dataFile;
     protected ArrayList<Boolean> colNotNull;
-    protected ArrayList<Boolean> isPrimary;
+    protected int primaryKey;
     protected ArrayList<Type> colTypes;
     protected ArrayList<String> colNames;
-    // Database database;
 
-    public Table(String table_name, ArrayList<Pair<String, Type>> cols) throws Exception {
+    /*
+      create a new table object
+     */
+    public Table(String table_name, String db_dir, 
+        ArrayList<Pair<String, Type>> cols) throws Exception {
         this.tableName = table_name;
+        this.baseDir = db_dir;
+        this.primaryKey = -1;
+        String prefix = this.baseDir + "/" + this.tableName;
+        if (new File(prefix + ".meta").exists() || new File(prefix + ".data").exists())
+            throw new Exception("table already exists.");
         
         HashSet<String> names = new HashSet<String>();
         for (Pair<String, Type> pair : cols) {
@@ -28,19 +36,31 @@ public class Table {
             names.add(pair.getKey());
             this.colTypes.add(pair.getValue());
             this.colNotNull.add(false);
-            this.isPrimary.add(false);
         }
         if (cols.size() != names.size()) throw new Exception("input names have duplicate.");
     }
 
-    public Table(String filename) 
+    /*
+      read a table from file
+     */
+    public Table(String table_name, String db_dir)
         throws IOException, Exception{
-            String prefix = DatabaseManager.getDatabasePath("test" + filename);
-            if (new File(prefix+".meta").exists() &&
-                new File(prefix+".data").exists()){
-                fromFile(filename);
+            this.tableName = table_name;
+            this.baseDir = db_dir;
+            this.primaryKey = -1;
+            String prefix = this.baseDir + "/" + this.tableName;
+            if (new File(prefix + ".meta").exists() &&
+                new File(prefix + ".data").exists()){
+                fromFile(prefix);
             }
-            else throw new Exception("file not exists.");
+            else throw new Exception("file not exist.");
+    }
+
+    /*
+      close a table, write thing back to storage
+    */
+    public void close() {
+
     }
 
     private void fromFile(String filename) {
@@ -51,12 +71,18 @@ public class Table {
 
     }
 
-    public void setPrimary(String col_name) {
-
+    public void setPrimary(String col_name) throws Exception{
+        for (int i = 0; i < this.colNames.size(); i++) {
+            if (col_name == this.colNames.get(i)) {
+                this.primaryKey = i;
+                break;
+            }
+        }
+        if (this.primaryKey == -1) throw new Exception("no match col names.");
     }
 
-    public void setNotNull(String col_name) {
-
+    public void setNotNull(ArrayList<Boolean> not_null) throws Exception{
+        // interface not decided
     }
 
 }
